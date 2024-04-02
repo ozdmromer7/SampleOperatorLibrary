3b574b97260f920a266d92a9b4a26182b043d62c
ozdmromer7 <omer.ozdemir@eryaz.net>
2024-04-02 18:34:13 +0300

    initial

---
+++ calculator/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.ozdemromer.calculator"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 31
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+}
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["release"])
+
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+
+            }
+        }
+    }
+}
---
+++ calculator/src/main/java/com/ozdemromer/calculator/Calculator.kt
@@ -0,0 +1,15 @@
+package com.ozdemromer.calculator
+
+class Calculator {
+
+    fun sum(num1 : Int, num2 : Int) : Int{
+        return num1 + num2;
+    }
+    fun sub(num1 : Int, num2 : Int) : Int{
+        return num1 - num2;
+    }
+
+    fun multiply(num1 : Int, num2 : Int) : Int{
+        return num1 * num2;
+    }
+}
